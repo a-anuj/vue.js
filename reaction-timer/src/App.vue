43e59acf73894e22762f421b09ac84f06972d1fc
a-anuj <anujamirthalingam5@gmail.com>
2024-12-16 22:06:56 +0530

    added a new Results component

--- reaction-timer/src/App.vue
+++ reaction-timer/src/App.vue
@@ -1,28 +1,36 @@
 <template>
   <h1>Reaction timer</h1>
   <button @click="showScreen"  :disabled="isShowing">Play</button>
-  <Block v-if="isShowing" :delay="delay"/>
+  <Block v-if="isShowing" :delay="delay" @end="endGame"/>
+  <Results v-if="showResults" :score="score"/>
 </template>
 
 <script>
 
 import Block from './components/Block.vue';
+import Results from './components/Results.vue'
 
 export default {
   name: 'App',
   data () {
     return {
       isShowing : false,
-      delay: null
+      delay: null,
+      score:null,
+      showResults : false,
     }
   },
-  components:{Block},
+  components:{Block, Results},
   methods: {
     showScreen()
     {
-      this.isShowing = !this.isShowing,
-      console.log(this.isShowing)
+      this.isShowing = true,
       this.delay = 2000 + Math.random()*5000
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime
+      this.isShowing = false
+      this.showResults = true
     }
   }
 }
@@ -42,5 +50,9 @@ h1{
 }
 button{
   margin-top: 40px;
+  margin-bottom: 40px;
+}
+p{
+  margin-top:30px
 }
 </style>
---
+++ reaction-timer/src/components/Results.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="result">
+    <h3>Reaction time : {{ score }} ms</h3>
+  </div>
+</template>
+
+<script>
+export default {
+    props:['score']
+}
+</script>
+
+<style>
+
+</style>
