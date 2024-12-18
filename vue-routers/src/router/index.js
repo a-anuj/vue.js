2619da8e9f220f9001217af76ff0fa2b9e8629a2
a-anuj <anujamirthalingam5@gmail.com>
2024-12-18 09:46:07 +0530

    Worked with 404 errors

--- vue-routers/src/router/index.js
+++ vue-routers/src/router/index.js
@@ -3,6 +3,7 @@ import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
 import Jobs from '@/views/jobs/Jobs.vue'
 import JobDetails from '@/views/jobs/JobDetails.vue'
+import NotFound from '@/views/NotFound.vue'
 
 const routes = [
   {
@@ -24,6 +25,11 @@ const routes = [
     path:'/jobs/:id',
     name:'JobDetails',
     component:JobDetails
+  },
+  {
+    path:'/:catchAll(.*)',
+    name:'notFound',
+    component:NotFound
   }
 ]
 
