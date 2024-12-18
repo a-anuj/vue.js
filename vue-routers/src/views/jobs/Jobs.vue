0367c3428005027834c86d08fa7ba2c74c95e8cf
a-anuj <anujamirthalingam5@gmail.com>
2024-12-18 09:18:21 +0530

    Created a new route for the jobs

---
+++ vue-routers/src/views/jobs/Jobs.vue
@@ -0,0 +1,22 @@
+<template>
+    <h1>Jobs</h1>
+    <h4 v-for="job in jobs" :key="job.id">{{ job.title }}</h4>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            jobs : [
+                { title : "Web Developer" ,id:1,details:"Frontend"},
+                {title : "App Developer",id:2,details:"Frontent"},
+                {title : "DataBase Organizer",id:3,details:"Backend"}
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
