49608f86c5489ed12f84d743cfd5d6f46a185569
a-anuj <anujamirthalingam5@gmail.com>
2024-12-18 16:21:03 +0530

    Added template refs

--- blog-page/src/views/HomeView.vue
+++ blog-page/src/views/HomeView.vue
@@ -1,24 +1,27 @@
 <template>
   <div class="home">
-    <h4>Name : {{ name }}</h4>
+    <h4 ref="p">Name : {{ name }}</h4>
     <h4>Age : {{ age }}</h4>
     <button @click="handleClick">Click me</button>
   </div>
 </template>
 
 <script>
+import { ref } from 'vue';
+
 
 export default {
   name: 'HomeView',
   setup() {
     let name = "Ram"
-    let age = 25
+    let age = 25 
 
+    const p = ref(null)
     const handleClick = () => {
-      console.log("Clicked")
+      console.log(p,p.value)
     }
 
-    return {name,age,handleClick}
+    return {name,age,handleClick,p}
   }
 }
 </script>
