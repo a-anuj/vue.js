bda18871d22bddd6330489f64257daa2c3c0f016
a-anuj <anujamirthalingam5@gmail.com>
2024-12-18 15:53:08 +0530

    customized the jobdetails page

--- vue-routers/src/views/jobs/JobDetails.vue
+++ vue-routers/src/views/jobs/JobDetails.vue
@@ -1,14 +1,30 @@
 <template>
-  <h1>Job Details here...</h1>
-  <p>Job ID is {{ $route.params.id }}</p>
+    <div v-if="job">
+        <h1>Job Details here...</h1>
+        <h3>{{ job.title }}</h3>
+        <h4>Job ID is {{ id }}</h4>
+        <p>Job Details : {{ job.details }}</p>
+    </div>
+
+    <div v-else>
+        <h3>Loading Details...</h3>
+    </div>
+  
 </template>
 
 <script>
 export default {
     data() {
         return {
-            id : this.$route.params.id
+            id : this.$route.params.id,
+            job:null
         }
+    },
+    mounted() {
+        fetch('http://localhost:3000/jobs/'+this.id)
+            .then(res=>res.json())
+            .then(data=> this.job=data)
+            .catch(err=>console.log(err.message))
     }
 }
 </script>
--- vue-routers/src/views/jobs/Jobs.vue
+++ vue-routers/src/views/jobs/Jobs.vue
@@ -1,10 +1,17 @@
 <template>
     <h1>Jobs</h1>
-    <div v-for="job in jobs" :key="job.id" class="job">
-        <router-link :to="{name : 'JobDetails', params:{ id: job.id } }">
-            <h3>{{ job.title }}</h3>
-        </router-link>
+    <div v-if="jobs.length">
+        <div v-for="job in jobs" :key="job.id" class="job">
+            <router-link :to="{name : 'JobDetails', params:{ id: job.id } }">
+                <h3>{{ job.title }}</h3>
+            </router-link>
+        </div>
     </div>
+
+    <div v-else>
+        <h3>Loading Jobs...</h3>
+    </div>
+    
 </template>
 
 <script>
